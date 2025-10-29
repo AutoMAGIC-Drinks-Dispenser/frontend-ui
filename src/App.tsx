ac8c600d632ce39af6cb0c675cdddd5adfcde349
Dolan <chdo@trifork.com>
2025-10-29 21:06:10 +0100

    adds login component as it has connection func

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,32 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { MainMenu } from './components/main_menu';
 import "./App.css";
 import "./index.css";
-// import { LoginPage } from './components/loginPage';
+import { LoginPage } from './components/loginPage';
 
 // Auth guard component
-// const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-//   const userId = sessionStorage.getItem('userId');
+const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const userId = sessionStorage.getItem('userId');
   
-//   if (!userId) {
-//     return <Navigate to="/" replace />;
-//   }
+  if (!userId) {
+    return <Navigate to="/" replace />;
+  }
   
-//   return <>{children}</>;
-// };
+  return <>{children}</>;
+};
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<MainMenu />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route 
+          path="/main" 
+          element={
+            // <ProtectedRoute>
+              <MainMenu />
+            // </ProtectedRoute>
+          } 
+        />
       </Routes>
     </Router>
   );
